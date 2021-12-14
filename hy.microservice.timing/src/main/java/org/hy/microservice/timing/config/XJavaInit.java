2a11474cda4bd844fc0a4ff523504c14f7a4dd9d
ZhengWei <HY.ZhengWei@qq.com>
2021-12-14 17:26:00 +0800

    1.0.0
    1. 基本版本完成

--- hy.microservice.timing/src/main/java/org/hy/microservice/timing/config/XJavaInit.java
+++ hy.microservice.timing/src/main/java/org/hy/microservice/timing/config/XJavaInit.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.hy.common.Help;
 import org.hy.common.app.Param;
+import org.hy.common.net.common.ClientCluster;
+import org.hy.common.thread.Jobs;
 import org.hy.common.xml.XJava;
 import org.hy.common.xml.log.Logger;
 import org.hy.common.xml.plugins.AppInitConfig;
@@ -64,12 +66,46 @@ public class XJavaInit extends AppInitConfig
                 this.init("config/timing/ms.timing.startup.Config.xml" ,this.xmlRoot);
                 this.init((List<Param>)XJava.getObject("StartupConfig_MS_Timing") ,this.xmlRoot);
                 this.init(((Param)XJava.getObject("MS_Timing_RootPackageName")).getValue());
+                
+                if ( i_IsStartJobs )
+                {
+                    this.init("config/timing/ms.timing.job.Config.xml" ,this.xmlRoot);
+                    
+                    // 注册：定时任务服务的灾备机制的心跳任务
+                    Jobs v_Jobs = (Jobs)XJava.getObject("MS_Timing_JOBS");
+                    XJava.putObject(Jobs.$JOB_DisasterRecoverys_Check ,v_Jobs.createDisasterRecoveryJob());
+                    
+                    this.init_ClusterServers();
+                }
             }
             catch (Exception exce)
             {
                 $Logger.error(exce);
             }
         }
+    }
+    
+    
+    
+    /**
+     * 通过灾备机制的服务列表，自动生成集群监控列表。不再麻烦用户配置两次
+     * 
+     * @author      ZhengWei(HY)
+     * @createDate  2021-12-14
+     * @version     v1.0
+     */
+    @SuppressWarnings("unchecked")
+    private void init_ClusterServers()
+    {
+        List<ClientCluster> v_DisasterRecoverys = (List<ClientCluster>)XJava.getObject("DisasterRecoverys");
+        StringBuilder       v_Buffer            = new StringBuilder();
+        for (ClientCluster v_Client : v_DisasterRecoverys)
+        {
+            v_Buffer.append(v_Client.getHost()).append(":").append(v_Client.getPort()).append(",");
+        }
+        
+        Param v_ClusterServers = new Param().setName("ClusterServers").setValue(v_Buffer.toString()).setComment("全部集群服务器列表(用逗号分隔)");
+        XJava.putObject(v_ClusterServers.getName() ,v_ClusterServers);
     }
 
 }
